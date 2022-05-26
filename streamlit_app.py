66d101769990fc38b5173a6d5237f76bae707d74
VINOD <106269447+vinodjovita@users.noreply.github.com>
2022-05-26 22:13:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-streamlit.title('My Parents New Healthy Dinners')
-streamlit.header('Breakfast Favorites')
+streamlit.title('My Parents New Healthy Dinners🐔🥑🍞')
+streamlit.header('🥣🥗Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
