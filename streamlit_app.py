bd6642eef15112d3671712750ca549f165a139c7
VINOD <106269447+vinodjovita@users.noreply.github.com>
2022-05-26 22:01:14 +0530

    Update streamlit_app.py
    update the hange

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinners')
-streamlit.title('Breakfast Menu')
-streamlit.title('Breakfa')
-
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry oatmeal')
+streamlit.text('Kale and smooth spina')
+streamlit.text('Hard Boild without egg yolk')
